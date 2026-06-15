678ba8fbf692d0e7ff27e72beaa155689ea091b0
pavanrambathina <pavanramkumarbathina@gmail.com>
2026-06-15 21:43:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if(sectionTop < windowHeight - 100){
+            section.classList.add("show");
+        }
+    });
+});
